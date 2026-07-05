888b6b91bdf93df2a29e1f058bae36fcddd7c187
Andre Bertok <53980377+Andrebtk@users.noreply.github.com>
2026-07-05 12:17:21 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        numberOfTimes = dict()
+
+        for letter in s:
+            if letter not in numberOfTimes:
+                numberOfTimes[letter] = 1
+            else:
+                numberOfTimes[letter] += 1
+
+        for letter in t:
+            if letter not in numberOfTimes:
+                return False
+            else:
+                numberOfTimes[letter] -= 1
+
+                if numberOfTimes[letter] < 0:
+                    return False
+        
+        for val in numberOfTimes.values():
+            if val != 0:
+                return False
+
+        return True
+        
+        
