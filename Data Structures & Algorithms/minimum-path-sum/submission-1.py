e36dc90b23c9c62b5ef7b97c4c4a60adb88b4bb9
Andre Bertok <53980377+Andrebtk@users.noreply.github.com>
2026-08-31 17:01:28 +0200

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ROWS, COLUMNS = len(grid), len(grid[0])
+
+        memo = [[-1] * COLUMNS for _ in range(ROWS)]
+        minPath = 9999999
+
+
+        def aux(r, c):
+            if r >= ROWS or c >= COLUMNS:
+                return 9999999
+            
+            if r == ROWS - 1 and c == COLUMNS - 1:
+                return grid[r][c]
+            
+            if memo[r][c] != -1:
+                return memo[r][c]
+            
+            memo[r][c] = min(aux(r + 1, c), aux(r, c + 1)) + grid[r][c]
+            return memo[r][c]
+
+        return aux(0, 0)
