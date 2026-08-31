a3e57613cc20082b53a29ce48336acd7ec4962bb
Andre Bertok <53980377+Andrebtk@users.noreply.github.com>
2026-08-31 16:41:06 +0200

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = [-1] * (n + 1)
+
+
+        def aux(i):
+            if i < 0:
+                return 0
+
+            if i == 0 or i == 1:
+                return i
+            
+            if i == 2:
+                return 1
+            
+            if memo[i] != -1:
+                return memo[i]
+            
+            memo[i] = aux(i-1) + aux(i-2) + aux(i-3)
+            return memo[i]
+
+        
+
+        return aux(n)
